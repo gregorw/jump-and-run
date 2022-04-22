089b42fe455f968117a90b8200d925e2526c8402
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-04-22 08:14:40 +0200

    Build

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: "src",
-  publicDir: "../dist",
+  publicDir: "../docs",
+  base: "/supermario/",
   plugins: [svelte()],
   server: {
     port: 5000
   },
   build: {
-    outDir: '../dist',
+    outDir: '../docs',
   }
 })
