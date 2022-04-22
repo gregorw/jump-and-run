86b266be751df0d692a21347a8550c2e10722ecb
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-04-22 19:37:41 +0200

    Build

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,8 @@
 	<aside style="text-align: center;">
 		<p>Please find the source code on <a href="https://github.com/gregorw/supermario">Github</a>.</p>
 		<p>
-			Can you make the player jumpy?<br>
+			Can you create your own world?<br>
+			Can you make the player jump?<br>
 			Can you detect collisions and display “Game Over”?
 		</p>
 	</aside>
