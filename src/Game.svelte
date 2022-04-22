1b9de45f2b5eea4db7490f5a6683cba58f5ce8eb
Gregor Wassmann <gregor.wassmann@gmail.com>
2022-04-22 11:32:48 +0200

    Add World with objects to Game

---
+++ src/Game.svelte
@@ -0,0 +1,39 @@
+<script>
+  import World from './World.svelte';
+	const units = 10;
+
+  const objects = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    ['🛢','🛢'],
+    ['🛢','🛢'],
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    ['☮️','☮️'],
+    ['☮️','☮️', '☮️'],
+    ['☮️','☮️'],
+    null,
+    null,
+    null,
+    ['🌵'],
+    ['🌵'],
+    null,
+    null,
+    null,
+  ]
+
+</script>
+
+<div class="game">
+  <World {objects} />
+</div>
---
+++ src/World.svelte
@@ -0,0 +1,15 @@
+<script>
+	export let objects = [];
+
+
+</script>
+
+<div class="world">
+  {#each objects as column, i}
+    <div class="column">
+      {#each column || [] as object}
+        <div class="object">{object}</div>
+      {/each}
+    </div>
+	{/each}
+</div>
